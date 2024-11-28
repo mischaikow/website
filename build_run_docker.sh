04311eed431e4b3e82cc9ce46b5371963f92c60d
mischaikow <mischaik@gmail.com>
2024-11-27 21:37:57 -0500

    fixed log permissions

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -12,7 +12,7 @@ cd /home/ec2-user/website
 docker buildx build --no-cache -t site/server -f Dockerfile .
 docker stop mischaikow-server
 docker run --rm -d -p 3000:3000 -e NODE_ENV=production \
-    -v /home/ec2-user/logs:/app/logs \
+    -v /home/ec2-user/logs:/app/logs:z \
     --network=mischaikow-home --name mischaikow-server --init site/server
 
 sleep 90s
