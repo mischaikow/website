978d5dcb79d18ac9d7b8da358af7e061562a92b1
mischaikow <mischaik@gmail.com>
2025-02-26 14:40:07 -0500

    getting pieces in place

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import * as index from './routes/index.js';
+import * as scoundrel from './routes/scoundrel.js';
 
 const router = Router();
 
 router.enableRender = (path: string) => router.get(`/${path}`, (_req, res) => res.render(`${path}.njk`));
 
 router.get('/', index.get);
+router.get('/scoundrel', scoundrel.get);
 
 export default router;
